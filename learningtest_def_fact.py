9a22c9193b707762daacbac4de9e9eceae94acc2
XMR <x_93@outlook.com>
2017-05-26 11:38:33 +0800

    modified runtest&fact

--- learningtest_def_fact.py
+++ learningtest_def_fact.py
@@ -1,5 +1,14 @@
+# -*- coding=utf-8 -*-
 def fact(n):
     if n == 1 :
         return 1
     return n*fact(n-1)
-print(fact(100))
+print(fact(10))
+
+def fact(n):
+    return dev_fact(n,1)
+def dev_fact(num,product):
+    if num==1:
+        return product
+    return dev_fact(num-1,product*num)
+print(fact(10))
