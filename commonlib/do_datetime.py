b0ef42b1b55528d213d0f402078dca30470d093a
MR <x_93@outlook.com>
2017-06-12 23:57:41 +0800

    modified datetime

--- commonlib/do_datetime.py
+++ commonlib/do_datetime.py
@@ -32,3 +32,23 @@ print(now)
 now=now+timedelta(hours=10)
 print(now)
 
+#作业
+# -*- coding:utf-8 -*-
+
+import re
+from datetime import datetime, timezone, timedelta
+
+def to_timestamp(dt_str, tz_str):
+    dt=re.match(r'^(1[0-9]{3}|200[0-9]|[201[0-7]|[0-9]{1,4})[: -\/_]+(0[0-9]|1[0-9]|2[0-3]|[0-9])\:(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|[0-9])\:(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|[0-9])',dt_str).group()
+    dt=datetime.strptime(dt)
+    dt=dt.
+
+# 测试:
+
+t1 = to_timestamp('2015-6-1 08:10:30', 'UTC+7:00')
+assert t1 == 1433121030.0, t1
+
+t2 = to_timestamp('2015-5-31 16:10:30', 'UTC-09:00')
+assert t2 == 1433121030.0, t2
+
+print('Pass')
